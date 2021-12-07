d31e66b7d5ead323498bef645cea94ec3b50fdb0
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-07 14:11:32 +0530

    [Ashika]REFACTORED: Display functional Component page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Props from './pages/props/Props';
 import State from './pages/state/State';
 import ClassComponent from './pages/class_component/ClassComponent';
+import FunctionalComponent from './pages/functional_component/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
       {/* <Props/> */}
       {/* <State/> */}
-      <ClassComponent/>
+      {/* <ClassComponent/> */}
+      <FunctionalComponent/>
     </div>
   );
 }
