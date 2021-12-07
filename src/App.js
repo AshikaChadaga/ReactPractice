a3ae616ee7428775b863fd3a78029ab773b5b2da
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-07 16:30:33 +0530

    [Ashika]REFACTORED: Displaying lifecycles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Props from './pages/props/Props';
 import State from './pages/state/State';
 import ClassComponent from './pages/class_component/ClassComponent';
 import FunctionalComponent from './pages/functional_component/FunctionalComponent';
+import ClassLifecycle from './pages/class_lifecycle/ClassLifecycle';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Props/> */}
       {/* <State/> */}
       {/* <ClassComponent/> */}
-      <FunctionalComponent/>
+      {/* <FunctionalComponent/> */}
+      <ClassLifecycle color="yellow"/>
     </div>
   );
 }
