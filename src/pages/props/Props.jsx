d4badb6d859aa61392f487e23346e1dc60b970d6
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-07 10:45:01 +0530

    [Ashika]REFACTORED: Function props demonstration

--- src/pages/props/Props.jsx
+++ src/pages/props/Props.jsx
@@ -3,11 +3,14 @@ import Greetings from '../../components/greeting/Greetings';
 
 function Props() {
     const greeting = "Good Morning";
+    const displayGreeting = () => {
+        return <h1>This is a function prop!</h1>
+    }
     return (
         <div>
             <Greetings greeting={greeting} />
             <Greetings greeting="Good Afternoon" />
-            <Greetings greetingObject={{ name: 'Ashika', message: 'Welcome to React Practice' }} />
+            <Greetings displayGreeting={displayGreeting()} greetingObject={{ name: 'Ashika', message: 'Welcome to React Practice' }} />
         </div>
     )
 }
