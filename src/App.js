40bff1d9b373812f28f7928a5071813657398bae
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-07 11:38:57 +0530

    [Ashika]REFACTORED: Display class Component page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Props from './pages/props/Props';
 import State from './pages/state/State';
+import ClassComponent from './pages/class_component/ClassComponent';
 
 function App() {
   return (
     <div className="App">
       {/* <Props/> */}
-      <State/>
+      {/* <State/> */}
+      <ClassComponent/>
     </div>
   );
 }
