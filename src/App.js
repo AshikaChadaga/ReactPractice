b5296b10b1eae449a9488259e2fea2ca4ecad52c
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-09 13:32:35 +0530

    [Ashika]REFACTORED: Demonstrate portals

--- src/App.js
+++ src/App.js
@@ -8,8 +8,31 @@ import FunctionLifecycle from './pages/function_lifecycle/FunctionLifecycle';
 import ReactHooks from './pages/react_hooks/ReactHooks';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
+import ReactPortal from './components/react_portal/ReactPortal';
+import Modal from './components/modal/Modal';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+  const [showBubbling, setShowBubbling] = useState(false);
+  const [count, setCount] = useState(0);
+  const handleShowMessageClick = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowBubblingClick = () => setShowBubbling(true);
+  const handleCloseBubbling = () => setShowBubbling(false);
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+  function Child() {
+    return (
+      <div className="modal">
+        <Button style={{marginRight:"30px"}} variant="contained">Click Me</Button>
+        <Button onClick={handleCloseBubbling} variant="contained">Close</Button>
+      </div>
+    );
+  }
   return (
     <div className="App">
       <Router>
@@ -22,8 +45,54 @@ function App() {
           <Route path="/class-lifecycle" element={<ClassLifecycle color="yellow" />}></Route>
           <Route path="/functional-lifecycle" element={<FunctionLifecycle />}></Route>
           <Route path="/react-hooks" element={<ReactHooks />}></Route>
+          {/* <Route path="/react-portal" element={<ReactPortal />}></Route> */}
         </Routes>
       </Router>
+      <div
+        style={{
+          height: '100%',
+          display: 'grid',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            maxWidth: 400,
+            position: 'relative',
+          }}
+        >
+          <h1>---------React Portal----------</h1>
+          <h2>My App</h2>
+          <p>
+            This is an example of how you might use React.createPortal. I think
+            it is a pretty neat API that is yet another awesome escape hatch
+            that React provides for practical reasons. Sometimes you just need
+            to render something completely outside the React Tree.
+          </p>
+          <Button onClick={handleShowMessageClick} variant="contained">Show Secret Modal</Button>
+          {showModal ? (
+            <ReactPortal onClose={handleCloseModal} handleClick={handleClick}>
+              This is the secret modal message!
+            </ReactPortal>
+          ) : null}
+        </div>
+      </div>
+      <div onClick={handleClick}>
+        <p>Number of clicks: {count}</p>
+        <p>
+          Open up the browser DevTools
+          to observe that the button
+          is not a child of the div
+          with the onClick handler.
+        </p>
+        <Button onClick={handleShowBubblingClick} variant="contained">Demonstrate Portal Event Bubbling</Button>
+        {showBubbling ? (<Modal>
+          <Child />
+        </Modal>
+        ) : null}
+
+      </div>
     </div>
   );
 }
