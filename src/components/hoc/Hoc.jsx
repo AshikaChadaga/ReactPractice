d0cee3b1359d99edd002a6ed2d664fdbc88e9f6c
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-09 14:01:35 +0530

    [Ashika]ADDED: Common HOC component

---
+++ src/components/hoc/Hoc.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+const Hoc = (Component, data) => {
+  //console.log("data", data);
+
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        count: data,
+      };
+    }
+
+    handleClick = () => {
+      this.setState({
+        count: this.state.count + 1,
+      });
+    };
+
+    render() {
+      return (
+        <Component
+          CountNumber={this.state.count}
+          handleCLick={this.handleClick}
+        />
+      );
+    }
+  };
+};
+
+export default Hoc;
